ef4c5680ceb29a4292bed6cc0919de55ddb92a0e
Namitha03 <namithasrinivas.03@gmail.com>
2024-04-23 17:50:07 +0530

    added new cors origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://blinkify.netlify.app/"],
     credentials: true,
   })
 );
